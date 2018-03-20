0cd4b6c96c9a1639bab1495826a33ec46e8dde63
Manish Kumar <master-fury@github.com>
2018-03-20 06:14:32 +0530

    Revert "update"
    This reverts commit e1c90b2bbe15c9c4438cda259ca68a949f50bf5b.

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://sagar:aakash@ds115749.mlab.com:15749/screenworks-test';
+
+var dbIns = false;
+
+var db;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Initialising the server
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine','ejs');
+
+//Handling the root get req.
+app.get('/',(req,res,next) => {
+//res.sendFile(__dirname + '/index.html');
+  db.collection('links').find().toArray(function(err,results) {
+    console.log(results);
+    res.render('index.ejs',{links:results});
+
+  });
+
+});
+
+MongoClient.connect(url, (err,client) => {
+  console.log(`Connected to ${url}`);
+  if(err) { console.log(err); }
+
+  db = client.db('screenworks-test');
+
+  app.listen(3000, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+});
+
+app.post('/links', (req,res) => {
+  db.collection('links').save(req.body,(err,result) => {
+    if(err) console.log(err);
+
+    console.log(`Link ${req.body.link} added`);
+    res.redirect('/');
+  })
+});
